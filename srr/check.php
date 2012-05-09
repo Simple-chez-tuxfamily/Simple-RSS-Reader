03b4ff32bee4c37322eac3c7150456306412f7f8
quent1-fr <quentin@quent1.fr>
2012-05-09 14:13:25 +0300

    Amélioration
    Amélioration de check.php: le vacuum est effectué à la fin et le die() est réalisé en premier.

--- srr/check.php
+++ srr/check.php
@@ -1,14 +1,12 @@
 <?php
+    if(!isset($_GET['id']) or !is_numeric($_GET['id'])){ die('Erreur: aucun id n\'a été spécifié!'); }
     set_time_limit(0);
     error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
-    if(!isset($_GET['id']) or !is_numeric($_GET['id'])){ die('Erreur: aucun id n\'a été spécifié!'); }
     include 'include/simplepie.inc';
     $simple = new SimplePie();
     $simple->enable_cache(false);
     $simple->set_useragent('Mozilla/4.0 '.SIMPLEPIE_USERAGENT.' (with Simple RSS Reader)');
     $sqlite = new PDO('sqlite:include/data.db');
-    $sqlite->query('DELETE FROM items WHERE read="1" AND user_id="' . $_GET['id'] . '"');
-    $sqlite->query('VACUUM');
     $query = $sqlite->query('SELECT id,url,last_check FROM feeds WHERE user_id="' . $_GET['id'] . '"');
     $time = time();
     while($response = $query->fetch()){
@@ -37,5 +35,7 @@
             $sqlite->query('UPDATE feeds SET last_check=' . $sqlite->quote($time) . ' WHERE id=' . $sqlite->quote($response['id']));
         }
     }
+    $sqlite->query('DELETE FROM items WHERE read="1" AND user_id="' . $_GET['id'] . '"');
+    $sqlite->query('VACUUM');
     header('Location: index.php');
 ?>
