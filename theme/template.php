0f273eaadc0af85a1e3d6c1cdaf24bc9a9aefbfa
Quentin Bouteiller <quentin@quent1.fr>
2014-01-10 22:18:29 +0100

    Support de Windows 8.1
    Meilleure intégration à Windows 8.1, avec des tuiles personnalisées.

--- theme/template.php
+++ theme/template.php
@@ -2,14 +2,22 @@
 <html>
     <head>
         <title><?php echo $title; ?> - Simple RSS Reader</title>
-        <link rel="shortcut icon" type="image/png" href="theme/images/favicon.png" />
-        <link rel="apple-touch-icon" href="theme/images/favicon.png" />
-        <meta name="viewport" content="initial-scale = 1.0,maximum-scale = 1.0" />
+        <link rel="shortcut icon" type="image/png" href="theme/images/favicon.png" />      
         <style type="text/css">
             @import url(http://fonts.googleapis.com/css?family=Open+Sans:400,300italic,700);
         </style>
         <link type="text/css" rel="stylesheet" href="core/css.php" />
         <script type="text/javascript" src="core/js.php"></script>
+        <!-- Balises pour les mobiles -->
+        <link rel="apple-touch-icon" href="theme/images/favicon.png" />
+        <meta name="viewport" content="initial-scale = 1.0,maximum-scale = 1.0" />
+        <!-- Balises pour l'intégration à Windows 8.1 -->
+        <meta name="application-name" content="Simple RSS Reader" />
+        <meta name="msapplication-TileColor" content="#8cb23a" />
+        <meta name="msapplication-square70x70logo" content="theme/images/w_70.png" />
+        <meta name="msapplication-square150x150logo" content="theme/images/w_1150.png" />
+        <meta name="msapplication-wide310x150logo" content="theme/images/w_3150.png" />
+        <meta name="msapplication-square310x310logo" content="theme/images/w_310.png" />
     </head>
     <body>
         <?php
